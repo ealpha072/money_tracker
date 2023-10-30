f7ea0bf43bc5b28d867da2c995c15d73469e43ff
ealpha072 <ealpha072@gmail.com>
2023-10-30 21:04:21 +0300

    Login page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,15 @@ import Accounts from './Pages/Accounts';
 import Dashboard from './Pages/Dashboard';
 import Transactions from './Pages/Transactions';
 import Settings from './Pages/Settings';
+import Login from './Pages/Login';
 
 const App = () => {
   return (
-    <div className='main-hoder'>
+    <div className='main-holder'>
       	<Router>
         	<Routes>
-				<Route path='/' element={<Wrapper componentProp={<Dashboard />}/>}></Route>
+				<Route path='/' element={<Login />}></Route>
+				<Route path='/dashboard' element={<Wrapper componentProp={<Dashboard />}/>}></Route>
             	<Route path='/account' element={<Wrapper componentProp={<Accounts />}/>}></Route>
 				<Route path='/transactions' element={<Wrapper componentProp={<Transactions />}/>}></Route>
 				<Route path='/settings' element={<Wrapper componentProp={<Settings />}/>}></Route>
---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+const Login = () => {
+  return (
+    <div class="container h-100 loginDiv">
+        <div class="row h-100 justify-content-center align-items-center ">
+            <div class="col-sm-6">
+                <h2 class="text-center">Login</h2>
+                <form>
+                    <div class="input-group input-group-lg mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i className="fa fa-user"></i></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                    </div>
+
+                    <div class="input-group input-group-lg mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i className="fa fa-unlock"></i></span>
+                        </div>
+                        <input type="password" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" />
+                    </div>
+
+                    <div className="mt-4 mb-4">
+                        <small id="passwordHelpInline" class="text-muted">
+                            No, account ? <Link to={"/"} className="text-success">Signup</Link>
+                        </small>
+                    </div>
+                   
+                    <button type="submit" class="btn btn-block btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+};
+
+export default Login
