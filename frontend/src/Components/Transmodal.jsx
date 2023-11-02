e0d1563b58050ae6c2ecefa8280f39d605fa5764
ealpha072 <ealpha072@gmail.com>
2023-11-02 22:45:58 +0300

    Control transaction modal tabs

--- frontend/src/Components/Transmodal.jsx
+++ frontend/src/Components/Transmodal.jsx
@@ -1,11 +1,18 @@
+import { Link } from "react-router-dom"
+import { useState } from "react"
 import Form from "./Form"
 
 const Transmodal = () => {
-    
+    const [activeLink, setActiveLink] = useState("Expense")
+
+    const handleLinkClick = (linkname) => {
+        setActiveLink(linkname)
+    }
+
     return (
         <div>
             <div className="modal fade" id="transModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered" role="document">
+                <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title" id="exampleModalLabel"><i className='fa fa-file'></i> New Account</h5>
@@ -13,11 +20,37 @@ const Transmodal = () => {
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <div className="modal-body">
-                            <div className="row">
-                                <div className="col bg-light pt-4 pb-4 border-right">Expenses</div>
-                                <div className="col bg-light pt-4 pb-4 border-right">Transfer</div>
-                                <div className="col bg-light pt-4 pb-4">Income</div>
+                        <div className="modal-body modal-card-body">
+                            <div className="row nav-link-div">
+                                <ul className="nav nav-tabs row" role="tablist">
+                                    <li className="nav-item col">
+                                        <Link 
+                                            className={`nav-link text-danger ${activeLink === "Expense" ? 'active' : ''}`} 
+                                            href="#" 
+                                            onClick={() => handleLinkClick("Expense")}
+                                        >
+                                            Expense
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item col">
+                                        <Link 
+                                            className={`nav-link ${activeLink === "Transfer" ? 'active' : ''}`}
+                                            href="#" 
+                                            onClick={() => handleLinkClick("Transfer")}
+                                        >
+                                            Transfer
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item col">
+                                        <Link 
+                                            className={`nav-link text-success ${activeLink === "Income" ? 'active' : ''}`}
+                                            href="#" 
+                                            onClick={() => handleLinkClick("Income")}
+                                        >
+                                            Income
+                                        </Link>
+                                    </li>
+                                </ul>
                             </div>
                             
                             <Form transfer="Transfer" buttonText="Create Account" />
--- frontend/src/Pages/Transactions.jsx
+++ frontend/src/Pages/Transactions.jsx
@@ -6,15 +6,15 @@ const Transactions = () => {
     <div>
         <div className="card">
             <div className="card-header">
-                <div class="btn-group bg-dark" role="group" aria-label="Basic example">
-                    <button type="button" data-toggle="modal" data-target="#transModal" class="btn btn-secondary"><i  className="fa fa-plus"></i></button>
-                    <button type="button" class="btn btn-secondary">New</button>
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div className="btn-group bg-dark" role="group" aria-label="Basic example">
+                    <button type="button" data-toggle="modal" data-target="#transModal" className="btn btn-secondary"><i  className="fa fa-plus"></i></button>
+                    <button type="button" className="btn btn-secondary">New</button>
+                    <div className="dropdown">
+                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i className='fa fa-calendar mr-4'></i>
                             29-08-2023
                         </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
                             <Link to={"#"} className="dropdown-item">Today</Link>
                             <Link to={"#"} className="dropdown-item">Yesterday</Link>
                             <Link to={"#"} className="dropdown-item">Last seven days</Link>
