f06d5c0c63b5d16b644575127ef95209a9bb6476
ealpha072 <ealpha072@gmail.com>
2023-11-22 10:30:00 +0300

    Restructured file

--- frontend/src/Redux/Reducers/Actions/actions.js
+++
@@ -1,7 +0,0 @@
-const transActionTypes = {
-
-}
-
-const accountActionTypes = {
-    
-}
---
+++ frontend/src/Redux/userSlice.js
@@ -0,0 +1,64 @@
+import { createSlice, createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
+import axios from 'axios'
+
+const baseUrl = "http://localhost:5000/user"
+
+export const signupUser = createAsyncThunk(
+    'user/signup',
+    async(data, thunkAPI) => {
+        try{
+            const response = await axios.post(`${baseUrl}/signup`, data)
+            console.log(response.data)
+        }catch (err){
+            return isRejectedWithValue(err.message)
+        }
+    }
+)
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState:{
+        userDetails:[],
+        isFetching:false,
+        isSuccess:false,
+        isError:false,
+        isLoggedIn:false,
+        isRegistered:false,
+        errorMessage:''
+    },
+    reducers:{
+        clearState: (state)=>{
+            state.isFetching = false
+            state.isError = false
+            return state
+        }, 
+        clearErrorMessage: (state)=>{
+            state.errorMessage=''
+        }
+    },
+    extraReducers:{
+        [signupUser.pending]: (state) =>{
+            state.isFetching = true
+        },
+        [signupUser.rejected]: (state, {payload}) => {
+            console.log(payload)
+            state.isFetching = false
+            state.isSuccess = false
+            state.isError = true
+            state.errorMessage = payload.error
+        },
+        [signupUser.fulfilled]: (state, {payload})=>{
+            state.isFetching = false
+            state.isSuccess = true
+            if(payload.error){
+                state.isError = true
+                state.errorMessage = payload.message
+            }else{
+                state.isRegistered = true
+            }
+        }
+    }
+})
+
+export const {clearErrorMessage, clearState} = userSlice.actions
+export const userSelector = state => state.user
